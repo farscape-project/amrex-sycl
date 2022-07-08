8d312370a9a8dc331ed1a08241678d995a10eade
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2022-07-08 14:47:28 +0100

    Add self-contained EM PIC example

---
+++ Exec/DPC++/build.sh
@@ -0,0 +1,18 @@
+AMREX_HOME=../../../../../../amrex
+
+CC=clang++
+CXX=clang++
+
+CXXFLAGS="-g1 -O3 -std=c++17 \
+-Wno-pass-failed -Wno-tautological-constant-compare -Wno-error=sycl-strict \
+-fsycl -fsycl-targets=nvptx64-nvidia-cuda -fsycl-device-code-split=per_kernel \
+-mlong-double-64 -Xclang -mlong-double-64 -pthread"
+
+USE_MPI=FALSE
+
+
+(cd "$AMREX_HOME" && patch -p0) < amrex.patch
+
+make CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" USE_MPI="$USE_MPI" $1
+
+(cd "$AMREX_HOME" && patch -R -p0) < amrex.patch

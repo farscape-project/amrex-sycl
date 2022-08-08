8d471266d40055c6f0b8d720107f0aeea0e168e0
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2022-08-08 20:43:17 +0100

    Add linker flags

--- Exec/SYCL/build.sh
+++ Exec/SYCL/build.sh
@@ -1,5 +1,9 @@
 AMREX_HOME=../../../../../../amrex
 
+###
+### DPC++
+###
+
 CC=clang++
 CXX=clang++
 
@@ -8,6 +12,12 @@ CXXFLAGS="-g1 -O3 -std=c++17 \
 -fsycl -fsycl-targets=nvptx64-nvidia-cuda -fsycl-device-code-split=per_kernel \
 -mlong-double-64 -Xclang -mlong-double-64 -pthread"
 
+LDFLAGS=-fsycl-device-lib=libc,libm-fp32,libm-fp64
+
+###
+### hipSYCL
+###
+
 CC=syclcc
 CXX=syclcc
 
@@ -15,11 +25,13 @@ CXXFLAGS="-g1 -O3 -std=c++17 \
 --hipsycl-targets=cuda:sm_70 \
 -pthread"
 
+LDFLAGS=
+
 USE_MPI=FALSE
 
 
 (cd "$AMREX_HOME" && patch -p0) < amrex.patch
 
-make CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" USE_MPI="$USE_MPI" "$@"
+make CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" USE_MPI="$USE_MPI" "$@"
 
 (cd "$AMREX_HOME" && patch -R -p0) < amrex.patch

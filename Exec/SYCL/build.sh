157575bc5612ea04bc4cb2862da290bf9685bca1
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2023-02-14 22:59:19 +0000

    Generalise build script for both AMD and NVIDIA GPUs

--- Exec/SYCL/build.sh
+++ Exec/SYCL/build.sh
@@ -1,38 +1,60 @@
 AMREX_HOME=../../../../../../amrex
 
+COMP=$( tr '[A-Z]' '[a-z]' <<< $1)
+
 ###
 ### DPC++
 ###
 
+if [[ $COMP == "dpcpp" || $COMP == "dpc++" ]]; then
+
 CC=clang++
 CXX=clang++
 
-CXXFLAGS="-g1 -O3 -std=c++17 -pthread \
--fgpu-inline-threshold=100000 \
--fsycl -fsycl-targets=nvptx64-nvidia-cuda \
--Xsycl-target-backend --cuda-gpu-arch=sm_70 \
--Xclang -mlong-double-64"
+CXXFLAGS="-g1 -O3 -std=c++17 -pthread -fgpu-inline-threshold=100000 \
+          -fsycl -Xclang -mlong-double-64"
 
-LDFLAGS=-fsycl-device-lib=libc,libm-fp32,libm-fp64
+if [[ $2 == "sm_"* ]]; then
+    CXXFLAGS=$CXXFLAGS" -fsycl-targets=nvptx64-nvidia-cuda \
+                        -Xsycl-target-backend --cuda-gpu-arch=$2"
+elif [[ $2 == "gfx"* ]]; then
+    CXXFLAGS=$CXXFLAGS" -fsycl-targets=amdgcn-amd-amdhsa \
+                        -Xsycl-target-backend --offload-arch=$2"
+fi
+
+LDFLAGS="-fsycl-device-lib=libc,libm-fp32,libm-fp64"
 
 ###
-### hipSYCL
+### Open SYCL
 ###
+
+elif [[ $COMP == "opensycl" || $COMP == "hipsycl" ]]; then
 
 CC=syclcc
 CXX=syclcc
 
-CXXFLAGS="-g1 -O3 -std=c++17 -pthread \
--fgpu-inline-threshold=100000 \
---hipsycl-targets=cuda:sm_70"
+CXXFLAGS="-g1 -O3 -std=c++17 -pthread -fgpu-inline-threshold=100000"
+
+if [[ $2 == "sm_"* ]]; then
+    CXXFLAGS=$CXXFLAGS" --hipsycl-targets=cuda:$2"
+elif [[ $2 == "gfx"* ]]; then
+    CXXFLAGS=$CXXFLAGS" --hipsycl-targets=hip:$2 -munsafe-fp-atomics"
+fi
 
 LDFLAGS=
 
-USE_MPI=FALSE
+###
+### Unrecognised compiler
+###
 
+else
+    echo "ERROR"
+fi
+
+USE_MPI=FALSE
 
 (cd "$AMREX_HOME" && patch -p0) < amrex.patch
 
-make DEPFLAGS= CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" USE_MPI="$USE_MPI" "$@"
+make DEPFLAGS= CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" USE_MPI="$USE_MPI" "${@:3}"
 
 (cd "$AMREX_HOME" && patch -R -p0) < amrex.patch
